5b2a1e42ecdddd13f50da512f85f2b6252894d07
liang chao <liangchao@m2plat.com>
2018-06-15 17:20:25 +0800

    安卓支持透明修改

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -67,7 +67,7 @@ public class StatusBar extends CordovaPlugin {
                 window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 
                 // Read 'StatusBarBackgroundColor' from config.xml, default is #000000.
-                setStatusBarBackgroundColor(preferences.getString("StatusBarBackgroundColor", "#000000"));
+                //setStatusBarBackgroundColor(preferences.getString("StatusBarBackgroundColor", "#000000"));
 
                 // Read 'StatusBarStyle' from config.xml, default is 'lightcontent'.
                 setStatusBarStyle(preferences.getString("StatusBarStyle", "lightcontent"));
