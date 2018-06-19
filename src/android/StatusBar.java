337b5788144acf17d90cf23bf3bf4890ac5cd96e
liang chao <liangchao@m2plat.com>
2018-06-19 14:11:32 +0800

    修改一下

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -23,6 +23,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Build;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 
